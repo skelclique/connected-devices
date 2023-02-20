ef257b1c95be601baffb63f093edb8098d619763
Vinicius Bortolo <luizviniciusbortolo@gmail.com>
2023-02-20 11:19:49 -0300

    create validation with zod validate post requested body

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,14 @@
+import { z } from 'zod';
 import { FastifyInstance } from "fastify";
 
 export async function routes(app: FastifyInstance) {
-  
+  app.post('/devices', async (request) => {
+    const createDevicesBody = z.object({
+      unformattedDevices: z.string(),
+      lanTemplate: z.string(),
+      wirelessTemplate: z.string(),
+    });
+
+    return createDevicesBody.parse(request.body);
+  });
 }
